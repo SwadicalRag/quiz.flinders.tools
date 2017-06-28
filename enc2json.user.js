87aa19f1ccea31c0ddf44dd01af6afcd528f2162
Swad <SwadicalRag@users.noreply.github.com>
2017-06-28 12:39:47 +0930

    some upgrades and swag

---
+++ enc2json.user.js
@@ -0,0 +1,112 @@
+// ==UserScript==
+// @name        ENC2JSON
+// @match   http://www.easynotecards.com/notecard_set/*
+// @match   https://www.easynotecards.com/notecard_set/*
+// @version     1
+// @downloadURL https://github.com/SwadicalRag/quiz.flinders.tools/raw/master/enc2json.user.js
+// @grant       GM_setClipboard
+// @run-at      document-end
+// ==/UserScript==
+
+function RPC_Clipboard(text) {
+    GM_setClipboard(text);
+};
+
+unsafeWindow.__flo_q2j_clip = exportFunction (RPC_Clipboard, unsafeWindow);
+
+function main() {
+    console.log("JA?");
+
+    let questions = [];
+
+    $(".vs-card").each(function() {
+        let question = {};
+
+        question.stem = $(this).find(".text > p").first().text().trim();
+        question.options = []; // {option: string,letter: string}[]
+
+        let correctOptionRaw = $(this).find(".def").text().trim();
+        let correctOption;
+
+        if(correctOptionRaw.match(/^The correct answer is: /)) {
+            correctOptionRaw = correctOptionRaw.match(/^The correct answer is: (.+)/)[1];
+        }
+        else if(correctOptionRaw.trim().match(/^[a-e]$/)) {
+            correctOption = correctOptionRaw.trim();
+        }
+        else if(correctOptionRaw.trim().match(/^([a-e])\. (.+)$/)) {
+            correctOption = correctOptionRaw.trim().match(/^([a-e])\. (.+)$/)[1];
+        }
+        else if(correctOptionRaw.trim().match(/^[1-5]$/)) {
+            correctOption = correctOptionRaw.trim();
+        }
+
+        question.correctOption = correctOption;
+
+        let idx = 1;
+        $(this).find(".text > ol > li").each(function() {
+            let text = $(this).text();
+
+            let optionText = text.trim();
+
+            question.options.push({
+                option: optionText,
+                letter: idx.toString(),
+            });
+
+            if(!correctOption && (optionText == correctOptionRaw)) {
+                question.correctOption = idx.toString();
+            }
+
+            idx++;
+        });
+
+        if(question.options.length == 0) {
+            $(this).find("p").each(function() {
+                let text = $(this).text();
+
+                let optionLetter = text.trim().match(/^[a-zA-Z]+/);
+                let optionText = text.trim().match(/^[a-zA-Z]+\.\s*(.+)/);
+
+                if(!(optionLetter && optionText)) {
+                    return;
+                }
+
+                optionLetter = optionLetter[0];
+                optionText = optionText[1];
+
+                question.options.push({
+                    option: optionText,
+                    letter: optionLetter
+                });
+
+                if(optionText == correctOptionRaw) {
+                    question.correctOption = optionLetter;
+                }
+            });
+        }
+
+        questions.push(question);
+    });
+
+    // console.log(questions);
+    function __internal_copyQuizToClipboard() {
+        __flo_q2j_clip(JSON.stringify(questions));
+    }
+    window.copyQuizToClipboard = __internal_copyQuizToClipboard;
+
+    $(".ui-grid-d.set-activities").append(
+        $("<div></div>").append(
+            $("<a></a>")
+                .attr("href","javascript:copyQuizToClipboard()")
+                .text("Copy")
+                .addClass("ui-link")
+        )
+        .addClass("ui-block-a")
+    );
+}
+
+// Call main() in the page scope
+var script = document.createElement("script");
+script.textContent = "(" + main.toString() + ")();";
+document.body.appendChild(script);
--- src/routes/QuizRoute.vue
+++ src/routes/QuizRoute.vue
@@ -9,7 +9,7 @@
                 </h5>
             </v-flex>
             <v-flex xs12>
-                <div v-for="answerData in options">
+                <div v-for="answerData in options" v-bind:key="answerData">
                     <b v-if="feedback && (question.data.correctOption == answerData.letter)">
                         <v-radio :disabled="!!feedback" :label="`${answerData.letter}. ${answerData.option}`" v-model="chosenAns" :value="answerData.letter" primary dark hide-details></v-radio>
                     </b>
@@ -38,10 +38,38 @@
                 <v-btn large primary light @click.native.stop="next">Next Question</v-btn>
             </v-flex>
         </v-layout>
+        <v-layout row wrap class="mt-5">
+            <v-flex xs12>
+                <v-card>
+                    <v-card-row class="orange darken-1">
+                        <v-card-title>
+                            <span class="white--text">Quiz Status</span>
+                        </v-card-title>
+                    </v-card-row>
+                    <v-card-text>
+                        <v-card-row height="75px">
+                            <v-icon class="mr-5" dark>card_membership</v-icon>
+                            <div>
+                                <div>Score</div><strong>{{score.correct}} / {{score.total}} ({{ (score.correct / (score.total || 1) * 100).toFixed(2) }}%)</strong>
+                            </div>
+                        </v-card-row>
+                    </v-card-text>
+                    <v-divider></v-divider>
+                    <v-card-row actions>
+                        <v-btn flat class="red--text darken-1" @click.native.stop="resetQuiz">Reset Quiz</v-btn>
+                        <v-btn flat class="orange--text darken-1" @click.native.stop="uniqueQuestions = !uniqueQuestions" v-if="uniqueQuestions">Disable Unique Questions</v-btn>
+                        <v-btn flat class="green--text darken-1" @click.native.stop="uniqueQuestions = !uniqueQuestions" v-if="!uniqueQuestions">Enable Unique Questions</v-btn>
+                    </v-card-row>
+                </v-card>
+            </v-flex>
+        </v-layout>
     </v-container>
 </template>
 
 <script>
+// import * as md5 from "md5";
+// ^ TODO: hash questions to preserve unique questions
+
 export default {
     name: "quizroute",
     data () {
@@ -49,10 +77,16 @@ export default {
             shared: window._SHARED,
             questionBank: [],
             options: [],
+            questionIdx: 0,
             question: false,
             chosenAns: "",
             correct: true,
             feedback: false,
+            uniqueQuestions: true,
+            score: {
+                total: 0,
+                correct: 0,
+            },
         }
     },
     methods: {
@@ -63,13 +97,17 @@ export default {
             return Math.floor(Math.random() * (max - min + 1)) + min;
         },
         randomQuestion() {
-            this.question = this.questionBank[this.getRandomIntInclusive(0,this.questionBank.length - 1)];
+            this.questionIdx = this.getRandomIntInclusive(0,this.questionBank.length - 1);
+            this.question = this.questionBank[this.questionIdx];
             this.options = JSON.parse(JSON.stringify(this.question.data.options)); // lazy asshole object copying
         },
         check() {
             if(this.chosenAns !== "") {
+                this.score.total++;
+
                 if(this.chosenAns == this.question.data.correctOption) {
                     this.correct = true;
+                    this.score.correct++;
                     this.feedback = "Correct!";
                 }
                 else {
@@ -90,28 +128,40 @@ export default {
         next() {
             this.feedback = false;
             this.chosenAns = "";
+
+            if(this.uniqueQuestions) {
+                this.questionBank.splice(this.questionIdx,1);
+            }
+
+            this.randomQuestion();
+        },
+        resetQuiz() {
+            let database = this.shared.getValue("question-db");
+
+            this.questionBank.splice(0,this.questionBank.length);
+
+            for(let i1=0;i1 < database.length;i1++) {
+                let quiz = database[i1];
+                if(!quiz.enabled) {continue;}
+                for(let i2=0;i2 < quiz.questions.length;i2++) {
+                    let question = quiz.questions[i2];
+
+                    this.questionBank.push({
+                        data: question,
+                        from: quiz.name,
+                    });
+                }
+            }
+
+            this.score.total = 0;
+            this.score.correct = 0;
+
             this.randomQuestion();
         }
     },
     created() {
-        let database = this.shared.getValue("question-db");
+        this.resetQuiz();
 
-        for(let i1=0;i1 < database.length;i1++) {
-            let quiz = database[i1];
-            if(!quiz.enabled) {continue;}
-            for(let i2=0;i2 < quiz.questions.length;i2++) {
-                let question = quiz.questions[i2];
-
-                this.questionBank.push({
-                    data: question,
-                    from: quiz.name,
-                });
-            }
-        }
-
-        console.log("OK")
-
-        this.randomQuestion();
         console.log(this.question)
     },
     mounted() {
